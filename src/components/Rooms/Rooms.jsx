70d18fa871bb756067cbb937775e32bf75558615
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-23 21:34:55 +0800

    room data loaded

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Rooms = () => {
+ const [rooms,setRooms]=useState([])
+ useEffect(()=>{
+    fetch("../../../public/rooms.json")
+    .then(res=>res.json())
+    .then(data=>setRooms(data))
+ },[])
+
+
+
     return (
         <div>
-           <h1>room card </h1> 
+          <h1>{rooms.length}</h1>
         </div>
     );
 };
